79a5fdd409672abb469e18e3baeb33fd5ce5544e
Emiliano <A01711141@tec.mx>
2026-04-23 17:14:52 -0600

    Funciones añadidas

--- Operaciones.c
+++ Operaciones.c
@@ -1,3 +1,8 @@
+int suma(int a, int b){
+ return a+b;
+}
+int resta(int a, int b){
+ r
 int main(){
 int num1 = 12;
 int num2 = 3;
@@ -5,7 +10,5 @@ int op1 = suma(num1,num2);
 printf("%i",op1);
 int op2 = resta(num1,num2);
 printf("%i",op2);
-int op3 = multi(num1,num2);
-printf("%i",op3);
 return 0;
 }
